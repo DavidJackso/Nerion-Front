1cc37657fbd06b8a83cde186deabc2f40737d87c
David Gergiev <gutnov3643@gmail.com>
2026-06-24 00:07:48 +0300

    feat: add head configuration for favicon in Nuxt app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    }
+  },
   modules: ['@pinia/nuxt'],
   css: ['~/assets/style.css'],
   runtimeConfig: {
